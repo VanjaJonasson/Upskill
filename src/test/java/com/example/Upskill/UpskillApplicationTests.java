b9f7889167809ffcf2c896c98cb6a0df263efb0d
VanjaJonasson <vanja_j@hotmail.com>
2022-06-27 14:11:03 +0200

    commit all files

---
+++ src/test/java/com/example/Upskill/UpskillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Upskill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpskillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
